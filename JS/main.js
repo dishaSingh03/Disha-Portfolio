76af0bb1cda4c58e945899d05f1d8a542f2712e9
dishaSingh03 <disha.singhh@gmail.com>
2022-08-12 02:15:18 -0700

    some_changes_and_work_section_added_to_portfolio

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,15 @@
 // Header Scroll
+
+// The querySelector() method returns the first element that matches a CSS selector.
+// To return all matches (not only the first), use the querySelectorAll() instead.
 let nav = document.querySelector(".navbar");
+
+// The onscroll event occurs when an element's scrollbar is being scrolled.
 window.onscroll = function () {
-    if(document.documentElement.scrollTop > 20){
+    if(document.documentElement.scrollTop > 30){
         nav.classList.add("header-scrolled");
     }else{
         nav.classList.remove("header-scrolled");
     }
 } 
+
